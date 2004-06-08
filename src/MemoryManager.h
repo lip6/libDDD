46c268d66ebae22edb56a70c04225b60fe7caa9b
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2004-06-08 13:20:26 +0000

    ajout SDD
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/trunk/Sources/TOOLS_C/COMPONENTS/tmp_libddd/libDDD@11 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- src/MemoryManager.h
+++ src/MemoryManager.h
@@ -4,6 +4,9 @@
 #include "DDD.h"
 #include "DED.h"
 #include "Hom.h"
+#include "SDD.h"
+#include "SDED.h"
+#include "SHom.h"
 
 class MemoryManager{
 public:
@@ -11,10 +14,18 @@ public:
   static unsigned int nbDDD(){return GDDD::statistics();};
   static unsigned int nbDED(){return DED::statistics();};
   static unsigned int nbHom(){return GHom::statistics();};
+  static unsigned int nbSDD(){return GSDD::statistics();};
+  static unsigned int nbSDED(){return SDED::statistics();};
+  static unsigned int nbShom(){return GShom::statistics();};
   /* Garbage Collector */
   static void mark(const GDDD &g){g.mark();};
   static void mark(const GHom &h){h.mark();};
   static void garbage(){
+    // FIXME : if you dont use SDD suppress the following
+    SDED::garbage();
+    GShom::garbage();
+    GSDD::garbage();
+    // END FIXME 
     DED::garbage();
     GHom::garbage();
     GDDD::garbage();
@@ -22,6 +33,13 @@ public:
 
   static void pstats(bool reinit=true){
     //cout << " Memory Usage " << MemUsage() << " %" << endl;
+    
+    // FIXME : if you dont use SDD suppress the following
+    SDED::pstats(reinit);
+    GShom::pstats(reinit);
+    GSDD::pstats(reinit);    
+    // END FIXME 
+
     DED::pstats(reinit);
     GHom::pstats(reinit);
     GDDD::pstats(reinit);    
