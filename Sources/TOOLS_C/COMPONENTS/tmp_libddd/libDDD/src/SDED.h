72ae40d0c7517df4b41ad42d21e60bbe033bcc5e
thierry <thierry@ab02c66d-5113-476d-a8b6-74e7035bd63a>
2004-06-08 13:05:06 +0000

    introducing SDD
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/trunk@10 ab02c66d-5113-476d-a8b6-74e7035bd63a

---
+++ Sources/TOOLS_C/COMPONENTS/tmp_libddd/libDDD/src/SDED.h
@@ -0,0 +1,81 @@
+/* -*- C++ -*- */
+#ifndef SDED_H
+#define SDED_H
+
+#include <set>
+#include "DataSet.h"
+
+
+class _SDED;
+class GSDD;
+class GShom;
+        
+/******************************************************************************/
+class SDED{
+private:
+  friend struct hash<SDED>;
+  friend struct equal_to<SDED>;
+  _SDED *concret;
+public:
+  GSDD eval();
+  SDED(_SDED *c):concret(c){};
+  bool operator==(const SDED&) const; 
+  // Currently undefined, + is only defined over two SDD not generalized over n
+//  static GSDD add(const set<GSDD> &);
+  static GSDD Shom(const GShom &,const GSDD&);
+
+  /* Memory Manager */
+  static  unsigned int statistics();
+  static void pstats(bool reinit=true);
+  static void garbage(); 
+};
+
+/* Binary operators */
+GSDD operator^(const GSDD&,const GSDD&); // concatenation
+GSDD operator+(const GSDD&,const GSDD&); // union
+GSDD operator*(const GSDD&,const GSDD&); // intersection
+GSDD operator-(const GSDD&,const GSDD&); // difference
+
+/******************************************************************************/
+namespace __gnu_cxx {
+  struct hash<SDED> {
+    size_t operator()(const SDED&) const;
+  };
+}
+
+namespace std {
+  struct equal_to<SDED> {
+    bool operator()(const SDED&,const SDED&) const;
+  };
+}
+
+/******************************************************************************/
+class _SDED{
+#ifdef INST_STL
+static long long NBJumps;
+static long long NBAccess;
+#endif
+
+public:
+  /* Destructor */
+  virtual ~_SDED(){};
+  virtual bool dogarbage() {return true;}
+  virtual bool shouldCache() { return true;}
+
+  /* Compare */
+  virtual size_t hash() const =0;
+  virtual bool operator==(const _SDED &) const=0;
+
+  /* Transform */
+  virtual GSDD eval() const=0; 
+
+#ifdef INST_STL
+  static void InstrumentNbJumps(int nboops){NBJumps+=(1+nboops);NBAccess++;}
+  static void ResetNbJumps(){NBJumps=0; NBAccess=0;}
+  static double StatJumps() {if (NBAccess!=0)  return double(NBJumps) / double(NBAccess); return -1;}
+#endif
+
+};
+
+#endif
+
---
+++ Sources/TOOLS_C/COMPONENTS/tmp_libddd/libDDD/src/SHom.h
@@ -0,0 +1,172 @@
+/* -*- C++ -*- */
+#ifndef SHOM_H_
+#define SHOM_H_
+
+#include "DataSet.h"
+#include "SDD.h"
+
+#include <string>
+#include <map>
+/**********************************************************************/
+#ifdef INST_STL
+typedef  std::pair<long long int, long long int> PairLL;
+typedef std::map<std::string, PairLL > MapJumps;
+#endif
+
+class _GShom;
+class StrongShom;
+
+class GShom {
+private:
+  friend class Shom;
+  friend struct hash<GShom>;
+  friend GShom operator+(const GShom &,const GShom &); 
+  friend GShom operator&(const GShom &,const GShom &); // composition
+  friend GShom operator*(const GSDD &,const GShom &); 
+  friend GShom operator*(const GShom &,const GSDD &); 
+  friend GShom operator^(const GSDD &,const GShom &); 
+  friend GShom operator^(const GShom &,const GSDD &); 
+  friend GShom operator-(const GShom &,const GSDD &); 
+  const _GShom* concret;
+  GShom(const _GShom *_h):concret(_h){};
+public:
+  /* Constructor */
+  GShom():concret(id.concret){};
+  GShom(const StrongShom *);
+  GShom(StrongShom *);
+  GShom(const GSDD& d);   // constant
+  GShom(int var,const DataSet& val, const GShom &h=GShom::id);  // var -- val -> Id
+
+  /* Elementary shomomorphisms */
+  static const GShom id;
+
+  /* Compare */
+  bool operator==(const GShom &h) const{return concret==h.concret;};
+  bool operator!=(const GShom &h) const{return concret!=h.concret;};
+  bool operator<(const GShom &h) const{return concret<h.concret;};
+
+  /* evaluation */ 
+  GSDD operator()(const GSDD &d) const; // evaluation
+  GSDD eval(const GSDD &d) const;
+
+  /* visualisation*/
+  int refCounter() const;
+
+  /* Operations */
+  static GShom add(const set<GShom>&);
+
+  /* Memory Manager */
+  static  unsigned int statistics();
+  static void pstats(bool reinit=true);
+  void mark()const;
+  static void garbage(); 
+};
+
+
+
+/* Operations */
+
+GShom operator+(const GShom &,const GShom &); 
+GShom operator&(const GShom &,const GShom &); // composition
+GShom operator*(const GSDD &,const GShom &); 
+GShom operator*(const GShom &,const GSDD &); 
+GShom operator^(const GSDD &,const GShom &); 
+GShom operator^(const GShom &,const GSDD &); 
+GShom operator-(const GShom &,const GSDD &); 
+
+
+class Shom:public GShom{
+public:
+  /* Constructor */
+  Shom(const GShom &h=GShom::id);
+  Shom(const Shom &h);
+  Shom(const GSDD& d);   // constant
+  Shom(int var,const DataSet& val, const GShom &h=GShom::id);  // var -- val -> Id
+  ~Shom();
+
+  /* Set */
+  
+  Shom &operator=(const GShom &);
+  Shom &operator=(const Shom &);
+};
+
+/******************************************************************************/
+namespace __gnu_cxx {
+  struct hash<GShom> {
+    size_t operator()(const GShom &g) const{
+      return (size_t) g.concret;
+    }
+  };
+}
+
+namespace std {
+  struct equal_to<GShom> {
+    bool operator()(const GShom &g1,const GShom &g2) const{
+      return g1==g2;
+    }
+  };
+}
+
+namespace std {
+  struct less<GShom> {
+    bool operator()(const GShom &g1,const GShom &g2) const{
+      return g1<g2;
+    }
+  };
+}
+
+/**********************************************************************/
+class _GShom{
+private:
+  friend class GShom;
+  friend class Shom;
+  mutable int refCounter;
+  mutable bool marking;
+  mutable bool immediat;
+public:
+#ifdef INST_STL
+  static MapJumps HomJumps;
+#endif
+  /* Destructor*/
+  _GShom(int ref=0,bool im=false):refCounter(ref),marking(false),immediat(im){};
+  virtual ~_GShom(){};
+
+  /* Compare */
+  virtual bool operator==(const _GShom &h) const=0;
+  virtual size_t hash() const=0;
+
+  /* Eval */
+  virtual GSDD eval(const GSDD &)const=0;
+
+  /* Memory Manager */
+  virtual void mark() const{};
+#ifdef INST_STL
+  virtual void InstrumentNbJumps(int nbjumps)
+  {
+    const char *name=typeid(*this).name();
+    MapJumps::iterator ii;
+    if ((ii=HomJumps.find(string(name)))==HomJumps.end()){
+      HomJumps[string(name)]=PairLL(1L, (long long int) (1+nbjumps));
+    }
+    else {
+      ii->second.first++;
+      ii->second.second+=(1+nbjumps);
+    }
+  }
+#endif
+};
+
+class StrongShom:public _GShom{
+public:
+  StrongShom(){};
+  virtual ~StrongShom(){};
+  virtual GSDD phiOne() const{return GSDD::top;}; 
+  virtual GShom phi(int var,const DataSet& val) const=0; 
+  virtual bool operator==(const StrongShom &h) const=0;
+  bool operator==(const _GShom &h) const;
+
+  //Eval
+  GSDD eval(const GSDD &)const;  
+};
+ 
+#endif /* SHOM_H_ */
