36b2b844ed75daab2db497992a62d9fced8e4362
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2005-02-17 20:17:57 +0000

    added message to remind valgrind usage syntax
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/trunk@57 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- Sources/TOOLS_C/COMPONENTS/tmp_libddd/libDDD/demo/tst11.cpp
+++ Sources/TOOLS_C/COMPONENTS/tmp_libddd/libDDD/demo/tst11.cpp
@@ -10,19 +10,14 @@ int main(){
 
   // Constants null, one , top
   cout <<"*****************************"<<endl;
-  cout <<"* Constants null, one , top *"<<endl;
+  cout <<"* Tracking memory leaks in SDD *"<<endl;
+  cout <<"* You probably wish to run : *"<<endl;
+  cout <<"valgrind --tool=memcheck --leak-check=yes tst11"<<endl;
   cout <<"*****************************"<<endl;
 
-  cout <<"DDD::null="<<endl<<DDD::null<<endl;
-  cout <<"DDD::one="<<endl<<DDD::one<<endl;
-  cout <<"DDD::top="<<endl<<DDD::top<<endl;
 
-  // Create a DDD: var -- val -> d
-  cout <<"***************"<<endl;
-  cout <<"* Create DDDs *"<<endl;
-  cout <<"***************"<<endl;
-
-  while(1) {
+  int i =0;
+  while(++i < 1000) {
             DDD d1 =  DDD(1,1);
             DDD d2 =  DDD(1,2);
             SDD s1 = SDD(1,d1);
