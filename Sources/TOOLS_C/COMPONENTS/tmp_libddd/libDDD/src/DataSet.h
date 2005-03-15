b494b10116caf1f6edb8ef7d58f901919d3b8c98
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2005-03-15 18:33:37 +0000

    updated comments to doxygen style
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/trunk@78 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- Sources/TOOLS_C/COMPONENTS/tmp_libddd/libDDD/src/DataSet.h
+++ Sources/TOOLS_C/COMPONENTS/tmp_libddd/libDDD/src/DataSet.h
@@ -9,31 +9,31 @@
 using namespace std;
 
 
-// Concrete DataSet classes should derive from DataSet and fulfill the contract FOR THEIR OWN TYPE
-// hard or dynamic casting the argument into one's own type is the recommended behavior 
+/// Concrete DataSet classes should derive from DataSet and fulfill the contract FOR THEIR OWN TYPE
+/// hard or dynamic casting the argument into one's own type is the recommended behavior 
 class DataSet {
  public :
-  // destructor
+  /// destructor
   virtual ~DataSet() {};
-  // returns a new instance copy of this 
+  /// returns a new instance copy of this 
   virtual DataSet *newcopy () const = 0;
-  // returns a new instance with elements = this inter b
+  /// returns a new instance with elements = this inter b
   virtual DataSet *set_intersect (const DataSet & b) const  = 0;
-  // returns a new instance with elements = this union b
+  /// returns a new instance with elements = this union b
   virtual DataSet *set_union (const DataSet & b)  const = 0;
-  // returns a new instance with elements = this setminus b
+  /// returns a new instance with elements = this setminus b
   virtual DataSet *set_minus (const DataSet & b) const = 0;
-  // returns true if this is the empty set
+  /// returns true if this is the empty set
   virtual bool empty() const = 0;
-  // returns a pointer to an instance of the empty set
+  /// returns a pointer to an instance of the empty set
   virtual DataSet *empty_set() const = 0;
-  // compares two sets for equality
+  /// compares two sets for equality
   virtual bool set_equal(const DataSet & b) const =0;
-  // returns the size (number of elements) in a set
+  /// returns the size (number of elements) in a set
   virtual long double set_size() const = 0;
-  // returns a hash function, used in the SDD hash function computation
+  /// returns a hash function, used in the SDD hash function computation
   virtual size_t set_hash() const =0;
-  // returns a formatted string description of the set
+  /// returns a formatted string description of the set
   virtual void set_print (ostream &os) const =0;
 };
 
