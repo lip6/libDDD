0d27dfda4f13dd744274d200505144b684e780c4
thierry <thierry@ab02c66d-5113-476d-a8b6-74e7035bd63a>
2005-09-09 12:42:32 +0000

    eliminated a heap of include + using namespace std also added dox-comments
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/trunk/Sources/TOOLS_C/COMPONENTS/tmp_libddd/libDDD@109 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- src/UniqueTable.h
+++ src/UniqueTable.h
@@ -2,44 +2,47 @@
 #ifndef UNIQUETABLE_H
 #define UNIQUETABLE_H
 
-// modif
-#include <ext/hash_set>
-#include <ext/hash_map>
-// ajout
-using namespace std;
-using namespace __gnu_cxx;
 
+#include <ext/hash_set>
 
 /// This class implements a unicity table mechanism, based on an STL hash_set.
-/// Requirements on the contained type are thus those of hash_set
+/// Requirements on the contained type are thus those of hash_set.
 template<typename T>
 class UniqueTable{
 
 #ifdef INST_STL
+  /// in the context of INST_STL version counts number of lookups in the table.
   long NbAcces;
+  /// In the context of INST_STL version, counts number of misses.
   long NbInsertion;
 #endif
 
 public:
-
+  /// Constructor, builds a default table.
   UniqueTable(){
 #ifdef INST_STL
     NbAcces=0;NbInsertion=0;
 #endif
   }
 
-  typedef hash_set<T*> Table;
+  /// Typedef helps hide implementation type (currently gnu gcc's hash_set).
+  typedef __gnu_cxx::hash_set<T*> Table;
+  /// The actual table, operations on the UniqueTable are delegated on this.
   Table table; // Unique table of GDDD
 
 /* Canonical */
+  /// The application operator, returns the address of the value already in 
+  /// the table if it exists, or inserts and returns the address of the value inserted.
+  /// \param _g the pointer to the value we want to find in the table.
+  /// \return the address of an object stored in the UniqueTable such that (*_g == *return_value)
   T *operator()(T *_g){
 #ifdef INST_STL
     NbAcces++;
     int nbjumps=0;
-    pair<typename Table::iterator, bool> ref=table.insert(_g, nbjumps); 
+    std::pair<typename Table::iterator, bool> ref=table.insert(_g, nbjumps); 
     _g->InstrumentNbJumps(nbjumps);
 #else
-    pair<typename Table::iterator, bool> ref=table.insert(_g); 
+    std::pair<typename Table::iterator, bool> ref=table.insert(_g); 
     
 #endif
   
@@ -57,11 +60,13 @@ public:
     return *ti;
   }
 
+  /// Returns the current number of filled entries in the table.
   size_t size() const{
     return table.size();
   }
 
 #ifdef INST_STL
+  /// Prints some statistics relating to UniqueTable effectiveness (only INST_STL version).
   void pstat(bool reinit=true){
     cout << "NbInsertion(" <<NbInsertion << ")*100/NbAccess(" << NbAcces<< ")  = " ;
     cout << ((long)(((long)NbInsertion) * 100)) / ((long)NbAcces) << endl;
