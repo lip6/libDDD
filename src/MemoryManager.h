cc0229e32e6baf430afbb1f68943b8ad25e1d90d
thierry <thierry@ab02c66d-5113-476d-a8b6-74e7035bd63a>
2005-09-09 12:35:32 +0000

    doxycumented version
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/trunk/Sources/TOOLS_C/COMPONENTS/tmp_libddd/libDDD@102 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- src/MemoryManager.h
+++ src/MemoryManager.h
@@ -8,18 +8,38 @@
 #include "SDED.h"
 #include "SHom.h"
 
+/// This class defines a few utility functions common to DDD.
+/// Note that all functions are declared static, so this is more of a namespace than a class.
+/// One important function is garbage(), only this MemoryManager::garbage() should be caled :
+/// avoid GHom::garbage(), DED::garbage(), GDDD::garbage() (and SDD versions of the same,
+/// they should not be called directly.
 class MemoryManager{
 public:
   /* Mesure*/
+  /// Returns the size of the unicity table for DDD.
   static unsigned int nbDDD(){return GDDD::statistics();};
+  /// Returns the size of the cache unicity table for DDD. 
   static unsigned int nbDED(){return DED::statistics();};
+  /// Returns the size of the unicity table for DDD Homomorphisms.
   static unsigned int nbHom(){return GHom::statistics();};
+  /// Returns the size of the unicity table for SDD.
   static unsigned int nbSDD(){return GSDD::statistics();};
+  /// Returns the size of the cache unicity table for SDD. 
   static unsigned int nbSDED(){return SDED::statistics();};
+  /// Returns the size of the unicity table for SDD Homomorphisms.
   static unsigned int nbShom(){return GShom::statistics();};
   /* Garbage Collector */
+  /// Convenience function to mark a node as non collectible.
+  /// \todo : track usage and check whether this is useful, SDD version undefined.  
   static void mark(const GDDD &g){g.mark();};
+  /// Convenience function to mark a Hom as non collectible.
+  /// \todo : track usage and check whether this is useful, SDD version undefined.  
   static void mark(const GHom &h){h.mark();};
+
+  /// Garbage collection function. 
+  /// Call this to reclaim intermediate nodes, unused operations and related cache.
+  /// Note that this function is quite costly, and it totally destroys the cache unless OTF 
+  /// garbage collection is activated.
   static void garbage(){
     // FIXME : if you dont use SDD suppress the following
     SDED::garbage();
@@ -31,6 +51,8 @@ public:
     GDDD::garbage();
   };
 
+  /// Prints some statistics about use of unicity tables, also reinitializes peak sizes.
+  /// Side effect in INST_STL version is to reinitialize rebound counters.
   static void pstats(bool reinit=true){
     //cout << " Memory Usage " << MemUsage() << " %" << endl;
     
