ec4478eba8f4b0d3037821c5b26e74ac10fa5bbc
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2007-06-19 22:21:22 +0000

    a recursive folded version but that breaks saturation
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/trunk/Sources/TOOLS_C/COMPONENTS/libDDD@149 ab02c66d-5113-476d-a8b6-74e7035bd63a

---
+++ demo/hanoi_v9.cpp
@@ -0,0 +1,191 @@
+/** An example resolution of the famous towers of Hanoi puzzle. *
+ * v8 : This variant exhibits the use IntDataSet based SDD, emphasizing the differences with DDD.
+*/
+
+
+#include <cstring>
+#include <string>
+#include <iostream>
+using namespace std;
+
+#include "IntDataSet.h"
+#include "DDD.h"
+#include "DED.h"
+#include "MemoryManager.h"
+
+// we use one DDD variable per ring, ring 0 is the topmost, 
+// and is stored at the bottom of the DDD
+static int NB_RINGS= 3;
+// Each variable domain is {0,1,2} expressing the pole the variable is on
+static int NB_POLES= 3;
+
+#define VAR_STATES 0
+#define VAR_HIER 1
+
+void initName() {
+  char buff [12];
+  for (int i=0; i< NB_RINGS; i++) {
+    sprintf(buff,"ring %d",i);
+    DDD::varName(i,buff);
+  }
+}
+
+// predeclaration
+GShom saturate ();
+  
+// Removes any path such that one of the variables takes value i or value j
+// Not test on variable number means this operation should be used from "mid-height"
+class _no_ring_above : public StrongShom {
+  // the 2 poles that have to be clear
+  IntDataSet set;
+public :
+  _no_ring_above (int i, int j) { 
+    // construct from vector
+    vector<int> v (2);
+    v[0] = i ;
+    v[1] = j;
+    set = IntDataSet(v);
+  }
+
+  GSDD phiOne() const {
+    return GSDD::one;
+  }     
+  
+  // reject any path with ANY ring that is on pole i or pole j
+  GShom phi(int vr, const DataSet & vl) const {
+    if (vr == VAR_STATES) {
+      // we know there is only one level of depth, therefore DataSet concrete type is IntDataSet
+      DataSet * tofree =  vl.set_minus(set);
+      IntDataSet res ( *( (IntDataSet *) tofree ) );
+      delete tofree;
+      
+      // test is useless, if res is empty SDD canonization of GShom(vr,res)(GSDD::one) returns GSDD::null node
+      if (! res.empty()) {
+	// usually we should
+	// propagate this test AND (re)saturate resulting nodes
+	// return GShom(vr,res, saturate() &GShom(this));
+	// but in fact successor should be GSDD::one so we know the result
+	return GShom(vr,res);
+      } else {
+	// cut this branch and exploration
+	return GSDD::null;
+      }
+    } else {
+      // propagate twice
+      SDD vl2 = GShom(this) ((const SDD &) vl);
+      return GShom (vr , vl2 ) & this ;
+    }
+  }
+
+  size_t hash() const {
+    return set.set_hash();
+  }
+
+  bool operator==(const StrongShom &s) const {
+    _no_ring_above* ps = (_no_ring_above*)&s;
+    return set.set_equal(ps->set );
+  }
+  
+};
+
+// generic version no ring specified, just apply to current ring
+// move constrained to use 2 specific poles
+class _move_ring : public StrongShom {
+  // the poles used
+  int p1,p2;
+public :
+
+  _move_ring(int pp1,int pp2): p1(pp1),p2(pp2) { assert (p1<p2); };
+  
+  GSDD phiOne() const {
+    return GSDD::one;
+  }                   
+  
+  GShom phi(int vr, const DataSet& vl) const {
+    // ring reached 
+    // try to move to all new positions
+    
+    if (vr == VAR_STATES) {
+      // Initialize res with Id
+      GShom res = GShom(vr,vl) ;
+
+      // concrete level reached : vl is an IntDataSet
+      for (IntDataSet::const_iterator vlit = ((const IntDataSet&)vl).begin() ; vlit != ((const IntDataSet&)vl).end() ; ++vlit ) {
+	if (*vlit == p1) {
+	  // move to p2
+	  res = res +  GShom (vr , IntDataSet(vector<int> (1,p2)));
+	} else if (*vlit == p2) {
+	  // move to p1
+	  res = res +  GShom (vr , IntDataSet(vector<int> (1,p1)));
+	}
+      }
+      return res ;
+    } else {
+      // hierarchy, vl is SDD
+      SDD vl2 = GShom(this) ((const SDD &) vl);
+      return (GShom (vr , vl2 ) & new _no_ring_above(p1 , p2))  + (GShom (vr , vl) & (this + GShom::id) );
+    }
+
+  }
+  
+  
+  size_t hash() const {
+    return 6961*p1+p2;
+  }
+  
+  bool operator==(const StrongShom &s) const {
+    return p1== ((const _move_ring &) s).p1 &&  p2== ((const _move_ring &) s).p2;
+  }
+  
+};
+
+// to be more pleasant for users  
+GShom move_ring (int i, int j ) {
+  return new _move_ring (i,j);
+}
+
+// "saturate" fires all events that can be fired from a given node to
+//  the leaves and returns a saturated node (à la Ciardo's RecFireAndSat).
+GShom saturate () {
+  GShom moves = GShom::id ;
+  for (int i=0; i < NB_POLES ; i++) {
+    for (int j=i+1; j < NB_POLES ; j++) {
+      moves = moves + move_ring(i,j);
+    }
+  }
+  return fixpoint(moves);
+}
+  
+  
+int main(int argc, char **argv){
+  if (argc == 2) {
+    NB_RINGS = atoi(argv[1]);
+  }
+
+  // Define a name for each variable
+  initName();
+
+  // The initial state
+  // construct an initial state for the problem, all rings are on pole 0
+  IntDataSet s (vector<int> (1,0) );
+  // User program variables should be DDD not GDDD, to prevent their garbage collection
+  SDD M0 = SDD(VAR_STATES, s);
+
+  for (int i=1; i<NB_RINGS ; i++ ) {
+    // tricky, recursive embedding
+    M0 = SDD(VAR_HIER ,M0, SDD(VAR_HIER ,M0) );
+  }
+  //  cout << M0 << endl ;
+
+  // Consider one single saturate event that recursively fires all events 
+  // Saturate topmost node <=> reach fixpoint over transition relation
+  SDD ss =  saturate() (M0) ;
+
+//  cout << ss << endl ;
+  // stats
+  cout << "Number of states : " << ss.nbStates() << endl ;
+  cout << "DDD Final/Peak nodes : " << ss.node_size().second << "/" << DDD::peak() << endl;
+  cout << "SDD Final/Peak nodes : " << ss.node_size().first << "/" << SDD::peak() << endl;
+  cout << "Cache entries DDD/SDD : " << MemoryManager::nbDED() <<  "/" <<  MemoryManager::nbSDED() << endl ;
+//  MemoryManager::pstats();
+}
