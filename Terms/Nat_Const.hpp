eda34f3997e9ad59b63aa859c3b54fec16d4b5eb
thierry <thierry@ab02c66d-5113-476d-a8b6-74e7035bd63a>
2007-07-02 16:34:41 +0000

    a basis with working 0 + x homomorphism
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/trunk/Sources/TOOLS_C/COMPONENTS/libDDD@151 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- Terms/Nat_Const.hpp
+++ Terms/Nat_Const.hpp
@@ -1,8 +1,32 @@
-// Variables type
-#define NAT 0
+#ifndef __NAT_CONST_HH__
+#define __NAT_CONST_HH__
 
-// symbolic arc values 
+#include "IntDataSet.h"
+#include "SDD.h"
+
+// SDD Variables 
+// Natural 
+#define NAT 0
+// Naturals expect an IntDataSet value representing one of the symbolic operators  if < 0 :
 #define PLUS -1
 #define SUC -2
+// else a constant 0, 1 , 2 ...
 
-// Constantes ont leur valeur en positif
+// left and right operands of binary operators (e.g. plus) : expect a SDD value
+#define LEFT 1
+#define RIGHT 2
+
+// a dataset for "+"
+extern const IntDataSet natPlus ;
+// a dataset for constant 0
+extern const IntDataSet natZero ;
+// a dataset for constant 1
+extern const IntDataSet natOne ;
+
+// a SDD dataset for constant 0
+extern const SDD SDDnatZero;
+// a SDD dataset for constant 1
+extern const SDD SDDnatOne;
+
+
+#endif // __NAT_CONST_HH__
