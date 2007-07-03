6a4033dc749f468703fd33344ca9038782614978
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2007-07-03 15:42:03 +0000

    removed now useless classes implemented by generic select
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/trunk/Sources/TOOLS_C/COMPONENTS/libDDD@160 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- Terms/Nat_PlusSucc.hpp
+++ Terms/Nat_PlusSucc.hpp
@@ -4,98 +4,6 @@
 #include "Nat_Const.hpp"
 #include "Hom_Select.hpp"
 
-// rewriting rule : X + succ(Y) -> succ( X+Y)
-class _select_succ : public StrongShom {
-public :
-  GSDD phiOne() const {
-    return GSDD::one;
-  }     
-  
-  // accept any NAT path with    X + succ( Y )  \forall X,Y
-  GShom phi(int vr, const DataSet & vl) const {
-    cout << " running select succ on vr=" << vr << " vl= " ; vl.set_print(cout) ; cout << endl ;
-    
-    if ( vr == NAT ) {
-      // we know there is only one level of depth, therefore DataSet concrete type is IntDataSet
-
-      // looks good : looking for  "succ" paths
-      // paths with anything except "succ" are killed
-      DataSet * tofree =  vl.set_intersect(natSucc);
-      if (! tofree->empty() ) {
-	delete tofree;
-	return SDDnatSucc ; // implicit id
-      } else {
-	// kill path
-	delete tofree;
-	return GSDD::null ;
-      }
-
-    } else {
-      assert (false);
-    }
-
-  }
-
-  size_t hash() const {
-    return 14347;
-  }
-
-  bool operator==(const StrongShom &s) const {
-    return true;
-  }
-
-
-};
-
-
-class _succ_plus_test : public StrongShom {
-
-public :
-
-  GSDD phiOne() const {
-    return GSDD::one;
-  }     
-  
-  // accept any NAT path with    X + succ( Y )  \forall X,Y
-  GShom phi(int vr, const DataSet & vl) const {
-    cout << " running succplus on vr=" << vr << " vl= " ; vl.set_print(cout) ; cout << endl ;
-
-
-    if (vr == NAT) {
-      // we know there is only one level of depth, therefore DataSet concrete type is IntDataSet
-
-      // looks good : looking for  "+" paths
-      // paths with anything except "PLUS" should be left alone
-      DataSet * tofree =  vl.set_minus(natPlus);
-      if ( tofree->empty() ) {
-	delete tofree;
-	return SDDnatPlus ^ this ;
-      } else {
-	// kill path
-	delete tofree;
-	return GSDD::null ;
-      }
-    } else if (vr == LEFT) {
-      // we know argument should be a NAT, therefore DataSet concrete type is SDD
-      
-      // store and continue
-      return GShom (vr, vl , this) ;
-    } else {
-      // vr == RIGHT
-      return GShom ( vr , SDD(GShom(new _select_succ()) ( (const SDD &) vl ) ));
-    }
-  }
-
-  size_t hash() const {
-    return 13127;
-  }
-
-  bool operator==(const StrongShom &s) const {
-    return true;
-  }
-  
-};
-
 class _succ_plus_X_stored : public StrongShom {
 
   GSDD X;
@@ -168,7 +76,6 @@ public :
 SDD applySuccPlusXY (const SDD & d) {
   GShom select_succ = select_hom(NAT, &natSucc, GShom::id);
   GShom select_plus_x_succ_y = select_hom(NAT, &natPlus, select_deephom(RIGHT, select_succ, GShom::id));
-  //  GSDD d1 = GShom(new _succ_plus_test()) (d);
   GSDD d1 = select_plus_x_succ_y(d);
   return GShom(new _succ_plus_XY()) (d1) 
     + (d-d1);
--- Terms/Nat_PlusZero.hpp
+++ Terms/Nat_PlusZero.hpp
@@ -4,63 +4,6 @@
 #include "Nat_Const.hpp"
 #include "Hom_Select.hpp"
 
-
-class _zero_plus_test : public StrongShom {
-  GShom h ;
-public :
-  _zero_plus_test (const GShom hh) : h(hh) {};
-
-  GSDD phiOne() const {
-    return GSDD::one;
-  }     
-  
-  // accept any NAT path with    0 +  X  \forall X
-  GShom phi(int vr, const DataSet & vl) const {
-    cout << " running zeroplus on vr=" << vr << " vl= " ; vl.set_print(cout) ; cout << endl ;
-
-
-    if (vr == NAT) {
-      // we know there is only one level of depth, therefore DataSet concrete type is IntDataSet
-
-      // looks good : looking for  "+" paths
-      // paths with anything except "PLUS" should be left alone
-      // dirty "delete" code due to use of DataSet interface instead of direct IntDataSet manipulation
-      DataSet * tofree =  vl.set_minus(natPlus);
-      if ( tofree->empty() ) {
-	delete tofree;
-	return SDDnatPlus ^ h ;
-      } else {
-	// kill path
-	delete tofree;
-	return GSDD::null ;
-      }
-    } else if (vr == LEFT) {
-      // we know argument should be a NAT, therefore DataSet concrete type is SDD
-      // look for paths with a zero
-      return GShom( vr , SDD (((const SDD &) vl) * SDDnatZero));
-
-    } else {
-      // should not reach this point ??
-      assert ( false );
-    }
-  }
-
-  void mark() const {
-    h.mark();
-  }  
-  
-  size_t hash() const {
-    return  ::__gnu_cxx::hash<GShom>()(h) ^ 12269; 
-  }
-
-  bool operator==(const StrongShom &s) const {
-    _zero_plus_test * ps = (_zero_plus_test *)&s;
-    return h ==  ps->h;
-  }  
-  
-};
-
-
 class _zero_plus_X : public StrongShom {
 
 public :
