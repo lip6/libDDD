2e289bcd257c7518c978c96983bbc7e1078902ed
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2007-07-03 13:23:22 +0000

    before refactoring for more generality
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/trunk/Sources/TOOLS_C/COMPONENTS/libDDD@154 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- Terms/Nat_Const.hpp
+++ Terms/Nat_Const.hpp
@@ -9,22 +9,31 @@
 #define NAT 0
 // Naturals expect an IntDataSet value representing one of the symbolic operators  if < 0 :
 #define PLUS -1
-#define SUC -2
+#define SUCC -2
 // else a constant 0, 1 , 2 ...
 
 // left and right operands of binary operators (e.g. plus) : expect a SDD value
-#define LEFT 1
-#define RIGHT 2
+#define LEFT 2
+#define RIGHT 1
+// argument of a succ
+#define SUCCARG 1
 
 // a dataset for "+"
 extern const IntDataSet natPlus ;
+// a dataset for constant "succ"
+extern const IntDataSet natSucc ;
+
+
 // a dataset for constant 0
 extern const IntDataSet natZero ;
 // a dataset for constant 1
 extern const IntDataSet natOne ;
 
+
 // a SDD dataset for constant +
 extern const SDD SDDnatPlus;
+// a SDD dataset for constant +
+extern const SDD SDDnatSucc;
 // a SDD dataset for constant 0
 extern const SDD SDDnatZero;
 // a SDD dataset for constant 1
