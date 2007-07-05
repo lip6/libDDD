d733c9f725d8e1d45a279352c8ae8ced26c7c992
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2007-07-05 14:17:45 +0000

    separated implem from spec
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/trunk/Sources/TOOLS_C/COMPONENTS/libDDD@178 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- Terms/Nat_OneSuccZero.hpp
+++ Terms/Nat_OneSuccZero.hpp
@@ -1,43 +1,8 @@
 #ifndef NAT_SUCC_ZERO__HH
 #define NAT_SUCC_ZERO__HH
 
-#include "Nat_Const.hpp"
-#include "Hom_Select.hpp"
+#include "SDD.h"
 
-
-class _one_to_succ_0 : public StrongShom {
-
-public :
-
-  GSDD phiOne() const {
-    return GSDD::one;
-  }     
-  
-  // accept any NAT path 1
-  GShom phi(int vr, const DataSet & vl) const {
-//    cout << " running one_to_succ0 on vr=" << vr << " vl= " ; vl.set_print(cout) ; cout << endl ;
-    assert (vr == NAT) ;
-    
-    // we know vl == {1} rewrite to succ(0)
-    return  SDD(SDDnatSucc ^ SDD ( SUCCARG, SDDnatZero));
-
-  }
-
-  size_t hash() const {
-    return 12289 ;
-  }
-
-  bool operator==(const StrongShom &s) const {
-    return true;
-  }
-  
-};
-
-SDD applyOneToSucc0 (const SDD & d) {
-  GShom select_one_test = select_hom(NAT, &natOne);
-  GSDD d1 = select_one_test(d);
-  return GShom(new _one_to_succ_0()) (d1) 
-    + (d-d1);
-}
+SDD applyOneToSucc0 (const SDD & d) ;
 
 #endif // NAT_SUCC_ZERO__HH
--- Terms/Nat_PlusZero.hpp
+++ Terms/Nat_PlusZero.hpp
@@ -4,20 +4,9 @@
 #include "Nat_Const.hpp"
 #include "Hom_Select.hpp"
 
-SDD applyZeroPlusX (const SDD & d) {
-  GShom select_zero_plus_test = select_hom(NAT, &natPlus, select_hom(LEFT, &SDDnatZero, GShom::id));
-  GSDD d1 = select_zero_plus_test(d);
-  return extract_value (RIGHT) (d1) 
-    + (d-d1);
-}
+SDD applyZeroPlusX (const SDD & d);
 
-SDD applyXPlusZero (const SDD & d) {
-  GShom select_plus_zero_test = select_hom(NAT, &natPlus, select_hom(RIGHT, &SDDnatZero, GShom::id));
-  GSDD d1 = select_plus_zero_test(d);
-  return extract_value (LEFT) (d1) 
-    + (d-d1);
-}
-
+SDD applyXPlusZero (const SDD & d);
 
 
 #endif // NAT_PLUS_ZERO__HH
