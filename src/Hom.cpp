d1e882c135a5025c970a47f2c1d6ab8750242254
thierry <thierry@ab02c66d-5113-476d-a8b6-74e7035bd63a>
2007-12-31 16:05:58 +0000

    moved application of F inside G application loop (Full saturation mechanism)
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/libddd/trunk@534 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- src/Hom.cpp
+++ src/Hom.cpp
@@ -530,11 +530,12 @@ public:
                     {
                         d1 = d2;
 
-                        // Apply ( Id + F )* on all sons
-                        d2 = F_part(d1);
-                    
+                        
                         // Apply ( G + Id )
                         for (std::set<GHom>::const_iterator it = partition.second.begin() ; it != partition.second.end() ; ++it ) {
+			  // Apply ( Id + F )* on all sons
+			  d2 = F_part(d2);
+                    
 			  d2 = (*it) (d2) + d2;
 			}
                     }
