bb2a732db190143f628270c6d274d9a4331f91b9
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2008-01-25 15:35:08 +0000

    added headers to files
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/libddd/trunk@581 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- demo/dotExporter.h
+++ demo/dotExporter.h
@@ -1,5 +1,49 @@
 /****************************************************************************/
 /*								            */
+/* This file is part of libDDD, a library for manipulation of DDD and SDD.  */
+/*     						                            */
+/*     Copyright (C) 2001-2008 Yann Thierry-Mieg, Jean-Michel Couvreur      */
+/*                             and Denis Poitrenaud                         */
+/*     						                            */
+/*     This program is free software; you can redistribute it and/or modify */
+/*     it under the terms of the GNU Lesser General Public License as       */
+/*     published by the Free Software Foundation; either version 3 of the   */
+/*     License, or (at your option) any later version.                      */
+/*     This program is distributed in the hope that it will be useful,      */
+/*     but WITHOUT ANY WARRANTY; without even the implied warranty of       */
+/*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        */
+/*     GNU LEsserGeneral Public License for more details.                   */
+/*     						                            */
+/* You should have received a copy of the GNU Lesser General Public License */
+/*     along with this program; if not, write to the Free Software          */
+/*Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+/*     						                            */
+/****************************************************************************/
+
+/****************************************************************************/
+/*								            */
+/* This file is part of libDDD, a library for manipulation of DDD and SDD.  */
+/*     						                            */
+/*     Copyright (C) 2001-2008 Yann Thierry-Mieg, Jean-Michel Couvreur      */
+/*                             and Denis Poitrenaud                         */
+/*     						                            */
+/*     This program is free software; you can redistribute it and/or modify */
+/*     it under the terms of the GNU Lesser General Public License as       */
+/*     published by the Free Software Foundation; either version 3 of the   */
+/*     License, or (at your option) any later version.                      */
+/*     This program is distributed in the hope that it will be useful,      */
+/*     but WITHOUT ANY WARRANTY; without even the implied warranty of       */
+/*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        */
+/*     GNU LEsserGeneral Public License for more details.                   */
+/*     						                            */
+/* You should have received a copy of the GNU Lesser General Public License */
+/*     along with this program; if not, write to the Free Software          */
+/*Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+/*     						                            */
+/****************************************************************************/
+
+/****************************************************************************/
+/*								            */
 /* This file is part of the PNDDD, Petri Net Data Decision Diagram  package.*/
 /*     						                            */
 /*     Copyright (C) 2004 Denis Poitrenaud and Yann Thierry-Mieg            */
