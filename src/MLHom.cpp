a870ed0806a8f64106149647168bbe0be686a9bb
thierry <thierry@ab02c66d-5113-476d-a8b6-74e7035bd63a>
2008-02-07 14:56:41 +0000

    implmented LeftConcat, add and GHom adapter and related functions
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/libddd/trunk@617 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- src/MLHom.cpp
+++ src/MLHom.cpp
@@ -77,6 +77,101 @@ public:
   }
 };
 
+class Add:public _MLHom{
+  std::set<MLHom> parameters;
+public:
+  /* Constructor */
+  Add(const std::set<MLHom> & s,int ref=0):_MLHom(ref),parameters(s){}
+
+  /* Compare */
+  bool operator==(const _MLHom &h) const{ 
+    return parameters==((Add*)&h )->parameters;
+  }
+  size_t hash() const { 
+    size_t res=0;
+    for(std::set<MLHom>::const_iterator gi=parameters.begin();gi!=parameters.end();++gi)
+      {
+	res ^= gi->hash();
+      }
+    return res;
+  }
+
+  bool
+  skip_variable(int) const 
+  {
+    return false;
+  }
+
+  /* Eval */
+  HomNodeMap eval(const GDDD &d) const { 
+    HomNodeMap m; 
+    for(std::set<MLHom>::const_iterator gi=parameters.begin();gi!=parameters.end();++gi)    
+      m.addAll( (*gi)(d)); 
+    return m; 
+  }
+};
+
+
+class GHomAdapter:public _MLHom{
+  GHom h;
+public:
+  /* Constructor */
+  GHomAdapter(const GHom & _h,int ref=0):_MLHom(ref),h(_h){}
+
+  /* Compare */
+  bool operator==(const _MLHom &other) const{ 
+    return h==((GHomAdapter*)&other )->h;
+  }
+  size_t hash() const { 
+    return  17449*h.hash();
+  }
+
+  bool
+  skip_variable(int var) const 
+  {
+    return h.skip_variable(var);
+  }
+
+  /* Eval */
+  HomNodeMap eval(const GDDD &d) const { 
+    HomNodeMap m; 
+    m.add(GHom::id,h(d)); 
+    return m; 
+  }
+};
+
+
+class LeftConcat:public _MLHom{
+  GDDD left;
+  MLHom h;
+public:
+  /* Constructor */
+  LeftConcat(const GDDD & l, const MLHom & _h,int ref=0):_MLHom(ref),left(l),h(_h){}
+
+  /* Compare */
+  bool operator==(const _MLHom &other) const{ 
+    return h==((LeftConcat*)&other )->h && left==((LeftConcat*)&other )->left;
+  }
+  size_t hash() const { 
+    return  19471*(h.hash()^left.hash());
+  }
+
+  bool
+  skip_variable(int var) const 
+  {
+    return false;
+  }
+
+  /* Eval */
+  HomNodeMap eval(const GDDD &d) const { 
+    HomNodeMap m = h(d);
+    HomNodeMap res;
+    for (HomNodeMap::const_iterator it = m.begin() ; it != m.end() ; ++it ){
+      res.add(it->first, left ^ it->second ); 
+    }
+    return res; 
+  }
+};
 
 
 
@@ -93,6 +188,13 @@ MLHom::~MLHom () {};
 MLHom::MLHom (const _MLHom *h):concret(h){};
 MLHom::MLHom (_MLHom *h):concret(canonical(h)){};
 
+MLHom::MLHom (const GHom &h):concret (canonical(new GHomAdapter(h))) {}
+
+MLHom::MLHom (int var, int val, const MLHom &h):concret(canonical(new LeftConcat(GDDD(var,val),h))){}
+
+HomNodeMap MLHom::operator() (const GDDD & d) const {
+  return concret->eval(d);
+}
 
 /************* Class StrongMLHom ***************/
 
@@ -120,4 +222,13 @@ HomNodeMap StrongMLHom::eval(const GDDD &d) const {
   }
   return res;
 
+}
+
+/********* operators *****************/
+
+MLHom operator+(const MLHom &h1,const MLHom &h2){
+  std::set<MLHom> s;
+  s.insert(h1);
+  s.insert(h2);
+  return canonical(new Add(s));
 }
