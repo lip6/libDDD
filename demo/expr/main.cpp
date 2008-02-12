68a860db87c19428f9397ad00b2972733f38ecc4
thierry <thierry@ab02c66d-5113-476d-a8b6-74e7035bd63a>
2008-02-12 16:49:15 +0000

    an example featuring boolean expressions
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/libddd/trunk@630 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- demo/expr/main.cpp
+++ demo/expr/main.cpp
@@ -1,4 +1,5 @@
 #include "IntExpression.hpp"
+#include "BoolExpression.hpp"
 
 #include <iostream>
 
@@ -16,13 +17,12 @@ int main () {
     Variable varb = Variable("b");
         
     IntExpression aexpr = IntExpressionFactory::createVariable(vara);
+    IntExpression bexpr = IntExpressionFactory::createVariable(varb);
     
     IntExpression aplus3 = aexpr + three;
-    
     cout << "a + 3 : " << aplus3 << endl;
     IntExpressionFactory::printStats(cout);
 
-    IntExpression bexpr = IntExpressionFactory::createVariable(varb);
     IntExpression aplus3b = aplus3 + bexpr;
     cout << "a + 3 + b : " << aplus3b << endl;
 
@@ -36,16 +36,30 @@ int main () {
     IntExpression four = IntExpressionFactory::createConstant(4);
 
     Assertion a = IntExpressionFactory::createAssertion (vara,four);
-
+    
     IntExpression fourplus6 = aplus3 & a ;
     cout << "a + 3 + 3 & a:4 " << fourplus6 << endl;
     cout << "(eval) : " << fourplus6.eval() << endl;
+    
+
+    BoolExpression acompb = aexpr == bexpr;
+    
+    BoolExpression alessb = aexpr < bexpr;
+    Assertion b = IntExpressionFactory::createAssertion (varb,four);
+    cout << acompb << endl
+	 << (acompb & a) << endl
+	 << (acompb & a & b) << endl
+	 << (acompb & a & b).eval() << endl
+	 << endl ;
+    
 
     cout << " In scope :" << endl;
     IntExpressionFactory::printStats(cout);
+    BoolExpressionFactory::printStats(cout);
   }
   cout << " Out of scope :" << endl;
-  IntExpressionFactory::printStats(cout);        
+  IntExpressionFactory::printStats(cout);   
+  BoolExpressionFactory::printStats(cout);     
 
   return 0;
 }
