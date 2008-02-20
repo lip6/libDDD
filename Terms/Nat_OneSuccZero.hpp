1684e1e7a968df84d76daf8ea632521a65c96b2e
thierry <thierry@ab02c66d-5113-476d-a8b6-74e7035bd63a>
2008-02-20 15:16:12 +0000

    moved to project terms at repository root
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/libddd/trunk@678 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- Terms/Nat_OneSuccZero.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef NAT_SUCC_ZERO__HH
-#define NAT_SUCC_ZERO__HH
-
-#include "SDD.h"
-
-SDD applyOneToSucc0 (const SDD & d) ;
-
-#endif // NAT_SUCC_ZERO__HH
--- Terms/Nat_PlusSucc.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef NAT_PLUS_SUCC__HH
-#define NAT_PLUS_SUCC__HH
-
-#include "SDD.h"
-
-SDD applySuccPlusXY (const SDD & d);
-
-#endif // NAT_PLUS_SUCC__HH
