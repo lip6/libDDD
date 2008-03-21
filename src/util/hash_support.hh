be55c0ce868750f6e9571bfeb99538da76dcc2e3
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2008-03-21 16:38:13 +0000

    added support to use std::pair of d3 types as key types in our hash_map
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/libddd/trunk@776 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- src/util/hash_support.hh
+++ src/util/hash_support.hh
@@ -1,6 +1,8 @@
 #ifndef _HASH_SUPPORT_HH_
 #define _HASH_SUPPORT_HH_
 
+#include <utility>
+
 namespace d3 { namespace util {
 
 template<typename T>
@@ -42,6 +44,25 @@ struct equal<T*>
     return (*e1)==(*e2);
   }
 };
+
+template <typename T1,typename T2>
+struct hash<std::pair<T1,T2> > {
+  size_t operator() (const std::pair<T1,T2> &p)const {
+    return hash<T1>()(p.first) ^ hash<T2>()(p.second);
+  };
+};
+
+
+template<typename T1,typename T2>
+struct equal<std::pair<T1,T2> >
+{
+  bool
+  operator()(const std::pair<T1,T2> & e1,const std::pair<T1,T2>& e2) const
+  {
+    return equal<T1>()(e1.first,e2.first) && equal<T2>()(e1.second,e2.second);
+  }
+};
+
 
 }}
 
