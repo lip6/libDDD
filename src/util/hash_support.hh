d5e8a3cd7140fb4df8e770130a3fdfc076645625
thierry <thierry@ab02c66d-5113-476d-a8b6-74e7035bd63a>
2008-03-25 15:04:27 +0000

    improve support for int as hash key. added comments & header.
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/libddd/trunk@779 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- src/util/hash_support.hh
+++ src/util/hash_support.hh
@@ -1,10 +1,32 @@
+/****************************************************************************/
+/*								            */
+/* This file is part of libDDD, a library for manipulation of DDD and SDD.  */
+/*     						                            */
+/*     Copyright (C) 2001-2008 Alexandre Hamez, Yann Thierry-Mieg           */
+/*     						                            */
+/*     This program is free software; you can redistribute it and/or modify */
+/*     it under the terms of the GNU Lesser General Public License as       */
+/*     published by the Free Software Foundation; either version 3 of the   */
+/*     License, or (at your option) any later version.                      */
+/*     This program is distributed in the hope that it will be useful,      */
+/*     but WITHOUT ANY WARRANTY; without even the implied warranty of       */
+/*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        */
+/*     GNU LEsserGeneral Public License for more details.                   */
+/*     						                            */
+/* You should have received a copy of the GNU Lesser General Public License */
+/*     along with this program; if not, write to the Free Software          */
+/*Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+/*     						                            */
+/****************************************************************************/
 #ifndef _HASH_SUPPORT_HH_
 #define _HASH_SUPPORT_HH_
 
 #include <utility>
+#include "hashfunc.hh"
 
 namespace d3 { namespace util {
 
+// Basic definition of a d3 hash function : define a member function : size_t hash() const , in hashable classes 
 template<typename T>
 struct hash
 {
@@ -15,6 +37,7 @@ struct hash
   }
 };
 
+// Basic definition of a d3 equality test : define a member function : bool operator== (const T & )const , in comparable classes 
 template<typename T>
 struct equal
 {
@@ -25,6 +48,7 @@ struct equal
   }
 };
 
+// Specialized version for storage of pointers to hashable objects. 
 template <typename T>
 struct hash<T*>
 {
@@ -35,6 +59,7 @@ struct hash<T*>
   }
 };
 
+// Specialized version for storage of pointers to comparable objects. Perform deep comparison.
 template<typename T>
 struct equal<T*>
 {
@@ -45,6 +70,15 @@ struct equal<T*>
   }
 };
 
+// Specialized version for use of int as keys in hash_map
+template<>
+struct hash<int> {
+  size_t operator()(int i) const {
+    return ddd::wang32_hash(i);
+  };
+ };
+
+// Specialized version for std::pair of hashable objects.
 template <typename T1,typename T2>
 struct hash<std::pair<T1,T2> > {
   size_t operator() (const std::pair<T1,T2> &p)const {
@@ -52,7 +86,7 @@ struct hash<std::pair<T1,T2> > {
   };
 };
 
-
+// Specialized version for std::pair of comparable objects.
 template<typename T1,typename T2>
 struct equal<std::pair<T1,T2> >
 {
