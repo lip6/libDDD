38d85364f771fb019c31968859e9515ba2d81d8c
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2008-11-24 12:22:32 +0000

    updated examples to use statistic.hpp
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/libddd/trunk@923 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- demo/tst1.cpp
+++ demo/tst1.cpp
@@ -24,78 +24,75 @@
 using namespace std;
 
 #include "DDD.h"
-#include "DED.h"
 #include "MemoryManager.h"
+#include "statistic.hpp"
 
 typedef enum {A, B, C, D,E, F, G} var;
 var variables;
 const char* vn[]= {"A", "B", "C", "D", "E", "F", "G"};
 
 void initName() {
-  for (int i=A; i<=G; i++)
-    DDD::varName(i,vn[i]);
+	for (int i=A; i<=G; i++)
+		DDD::varName(i,vn[i]);
 }
 
 int main(){
-  // Define a name for each variable
-  initName();
+	// Define a name for each variable
+	initName();
 
-  // Constants null, one , top
-  cout <<"*****************************"<<endl;
-  cout <<"* Constants null, one , top *"<<endl;
-  cout <<"*****************************"<<endl;
+	// Constants null, one , top
+	cout <<"*****************************"<<endl;
+	cout <<"* Constants null, one , top *"<<endl;
+	cout <<"*****************************"<<endl;
 
-  cout <<"DDD::null="<<endl<<DDD::null<<endl;
-  cout <<"DDD::one="<<endl<<DDD::one<<endl;
-  cout <<"DDD::top="<<endl<<DDD::top<<endl;
+	cout <<"DDD::null="<<endl<<DDD::null<<endl;
+	cout <<"DDD::one="<<endl<<DDD::one<<endl;
+	cout <<"DDD::top="<<endl<<DDD::top<<endl;
 
-  // Create a DDD: var -- val -> d
-  cout <<"***************"<<endl;
-  cout <<"* Create DDDs *"<<endl;
-  cout <<"***************"<<endl;
+	// Create a DDD: var -- val -> d
+	cout <<"***************"<<endl;
+	cout <<"* Create DDDs *"<<endl;
+	cout <<"***************"<<endl;
 
-  DDD a(A,1);
-  DDD b(A,1,DDD(B,2));
-  DDD c=DDD(A,1,DDD(A,1))+DDD(A,2,DDD(B,1));
-  DDD d=DDD(A,1,DDD(A,1))+DDD(A,2,DDD(B,2));
-  DDD e=DDD(A,1,5,c);
+	DDD a(A,1);
+	DDD b(A,1,DDD(B,2));
+	DDD c=DDD(A,1,DDD(A,1))+DDD(A,2,DDD(B,1));
+	DDD d=DDD(A,1,DDD(A,1))+DDD(A,2,DDD(B,2));
+	DDD e=DDD(A,1,5,c);
 
-  cout <<"a = A-1-><1> ="<<endl<<a<<endl;
-  cout <<"b = A-1->B-2-><1> ="<<endl<<b<<endl;
-  cout <<"c = A-1->A-1-><1> + A-2->B-1-><1> ="<< endl<<c<<endl;
-  cout <<"d = A-1->A-1-><1> + A-2->B-2-><1>"<< endl<<d<<endl;
-  cout <<"e = A-[1,5]->c ="<< endl<<e<<endl;
+	cout <<"a = A-1-><1> ="<<endl<<a<<endl;
+	cout <<"b = A-1->B-2-><1> ="<<endl<<b<<endl;
+	cout <<"c = A-1->A-1-><1> + A-2->B-1-><1> ="<< endl<<c<<endl;
+	cout <<"d = A-1->A-1-><1> + A-2->B-2-><1>"<< endl<<d<<endl;
+	cout <<"e = A-[1,5]->c ="<< endl<<e<<endl;
 
-  // Operations + (union), * (intersection), ^ (concatenation)
-  cout <<"*************************************************************"<<endl;
-  cout <<"* Operations + (union), * (intersection), ^ (concatenation) *"<<endl;
-  cout <<"*************************************************************"<<endl;
+	// Operations + (union), * (intersection), ^ (concatenation)
+	cout <<"*************************************************************"<<endl;
+	cout <<"* Operations + (union), * (intersection), ^ (concatenation) *"<<endl;
+	cout <<"*************************************************************"<<endl;
 
-  cout <<"c+d="<<endl<<c+d<<endl;
-  cout <<"c*d ="<<endl<<c*d<<endl;
-  cout <<"c-d ="<<endl<<c-d<<endl;
-  cout <<"a^c ="<<endl<<(a^c)<<endl;
-  cout <<"c^a ="<<endl<<(c^a)<<endl;
-  cout <<"c^d ="<<endl<<(c^d)<<endl;
-
-
-  DDD f(A,1,DDD(B,1));
-  DDD g(A,1,DDD(A,1));
-  DDD h = f+g;
-  cout << h <<endl;
-
-  cout<<MemoryManager::nbDDD()<<endl;
-  cout<<MemoryManager::nbHom()<<endl;
-  cout<<MemoryManager::nbDED()<<endl<<endl;
-
-  MemoryManager::garbage();
-  
-  cout<<MemoryManager::nbDDD()<<endl;
-  cout<<MemoryManager::nbHom()<<endl;
-  cout<<MemoryManager::nbDED()<<endl;
-
-  MemoryManager::pstats();
+	cout <<"c+d="<<endl<<c+d<<endl;
+	cout <<"c*d ="<<endl<<c*d<<endl;
+	cout <<"c-d ="<<endl<<c-d<<endl;
+	cout <<"a^c ="<<endl<<(a^c)<<endl;
+	cout <<"c^a ="<<endl<<(c^a)<<endl;
+	cout <<"c^d ="<<endl<<(c^d)<<endl;
 
 
-  return 1;
+	DDD f(A,1,DDD(B,1));
+	DDD g(A,1,DDD(A,1));
+	DDD h = f+g;
+	cout << h <<endl;
+
+	Statistic s(h,"b4 garbage",CSV);
+
+	MemoryManager::garbage();
+
+	Statistic s2(h,"after garbage",CSV);
+
+	s.print_header(std::cout);
+	s.print_line(std::cout);
+	s2.print_line(std::cout);
+
+	return 1;
 }
