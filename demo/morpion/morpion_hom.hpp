15f64572bb108900a1e3d7e30feae4354ae9fe87
shong <shong@ab02c66d-5113-476d-a8b6-74e7035bd63a>
2009-01-20 14:28:11 +0000

    Version 1 OK
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/libddd/trunk@1307 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- demo/morpion/morpion_hom.hpp
+++ demo/morpion/morpion_hom.hpp
@@ -7,13 +7,21 @@
 #include "Hom.h"
 #include "SDD.h"
 #include "SHom.h"
+#include <iostream>
+
+
+
 
 
 extern const int EMPTY;
+extern const int NBCASE;
+extern const int LINE;
+extern const int COLUMN;
+
 // play a move
 GHom take_cell (int cell, int player) ;
 
 GHom SelectWin (int c1, int c2, int c3, int nb, int player);
 
-
+GHom checkWinner (int t[3][3]);
 #endif 
