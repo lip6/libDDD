c687a41ef98a1c543c1d92017ac0c78a28305268
thierry <thierry@ab02c66d-5113-476d-a8b6-74e7035bd63a>
2009-01-25 15:50:13 +0000

    add checkNoWinner, sort the functions a bit
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/libddd/trunk@1342 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- demo/morpion/morpion_hom_v2.hpp
+++ demo/morpion/morpion_hom_v2.hpp
@@ -15,15 +15,13 @@ extern const int EMPTY;
 extern const int NBCASE;
 extern const int LINE;
 extern const int COLUMN;
+// players
+extern const int PA ;
+extern const int PB ;
 
+// update the variable representing game status to "player" wins
 GHom
     NoteWinner (int player);
-
-GHom 
-    CheckCellNoWinner (int player,int cell);
-
-GHom 
-    Play ( int c1, int player);
 
 // The player designated plays in a free cell, if any are available. 
 GHom 
@@ -32,6 +30,17 @@ GHom
 // Select any path where the designated player has won : i.e has three aligned cross or circle.
 GHom 
      CheckIsWinner (int player);
+
+// Selects paths such that there is no winner
+GHom CheckNoWinner ();
+
+/// low level support homomorphisms
+GHom 
+    CheckCellNoWinner (int player,int cell);
+
+GHom 
+    Play ( int c1, int player);
+
 
 GHom 
     CheckCellWinner (int player , int cell);
