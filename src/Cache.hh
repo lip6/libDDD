7aa3736ea63c24981f90e59bf485221d8d7f6801
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2009-04-28 17:42:41 +0000

    back to basics ! remove all REENTRANT #ifdef code. Code is still reentrant, but internal parallelism should not be implemented on this version.
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/libddd/trunk@1576 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- src/Cache.hh
+++ src/Cache.hh
@@ -14,30 +14,10 @@ class Cache
 {
 private:
   mutable size_t peak_;
-
-    struct result_type
-    {
-        NodeType node_;
-#ifdef REENTRANT
-        tbb::mutex mutex_;
-        bool computed_;
-#endif 
-        
-        result_type()
-            : node_()
-#ifdef REENTRANT
-            , mutex_()
-            , computed_(false)
-#endif
-        {
-        }
-        
-    };
-
-    typedef
-        typename  hash_map< std::pair<HomType, NodeType>, result_type >::type 
-        hash_map; 
-    hash_map cache_;
+  
+  typedef typename  hash_map< std::pair<HomType, NodeType>, NodeType >::type 
+                    hash_map; 
+  hash_map cache_;
     
 public:
   Cache () : peak_ (0) {};
@@ -63,45 +43,27 @@ public:
     std::pair<bool,NodeType>
     insert(const HomType& hom, const NodeType& node)
     {
-        
-#ifdef REENTRANT
-        tbb::mutex::scoped_lock lock;
-#endif
-        result_type* result = NULL;
-        bool insertion;
-        
-        { // lock on current bucket
-            typename hash_map::accessor access;
-            insertion = cache_.insert(access, std::make_pair(hom,node));
-            result = &(access->second);
-#ifdef REENTRANT
-            if( insertion )
-            {
-                lock.acquire(result->mutex_);
-            }
-#endif
-        } // end of lock on the current bucket
-        
-        if( insertion )
-        {   // wasn't in cache
-            result->node_ = hom.eval(node);
-#ifdef REENTRANT
-            result->computed_ = true;
-            lock.release();
-#endif            
-        }
-#ifdef REENTRANT
-        else
-        {   // was in cache
-            if( not result->computed_ )
-            {   // need to wait for the end of computation by first thread
-                // arrived into the cache
-                lock.acquire(result->mutex_);
-            }
-        }
-#endif
-        return std::make_pair(insertion,result->node_);
+      bool found;
+      
+      { // lock on current bucket
+	typename hash_map::const_accessor access;
+	found = cache_.find ( access, std::make_pair(hom,node));
+	if (found)
+	  return std::make_pair(false, access->second);
+      } // end of lock on the current bucket
+      
+      // wasn't in cache
+      NodeType result = hom.eval(node);
+      // lock on current bucket
+      typename hash_map::accessor access;
+      bool insertion = cache_.insert ( access, std::make_pair(hom,node));
+      if (insertion) {
+	// should happen except in MT case
+	access->second = result;
+      }
+      return std::make_pair(insertion,result);
     }
+  
 
     
 };
