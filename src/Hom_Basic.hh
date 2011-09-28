196d800500cefa5822f727e1210f19304ee20cb0
Maximilien Colange <colange@lrde.epita.fr>
2011-09-28 12:58:25 +0000

    enhanced version taken from libits
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/libddd/trunk@2685 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- src/Hom_Basic.hh
+++ src/Hom_Basic.hh
@@ -24,6 +24,17 @@ GHom setVarConst (int var, int val) ;
 // increment or decrement the value of var by val
 GHom incVar (int var, int val) ;
 
-
+// keep paths where variable var is equal to val
+GHom varEqVar (int var, int var2) ;
+// keep paths where variable var is NOT equal to var2
+GHom varNeqVar (int var, int var2) ;
+// keep paths where variable var is strictly greater than var2
+GHom varGtVar (int var, int var2) ;
+// keep paths where variable var is strictly < var2
+GHom varLtVar (int var, int var2) ;
+// keep paths where variable var is <= to var2
+GHom varLeqVar (int var, int var2) ;
+// keep paths where variable var is >= to var2
+GHom varGeqVar (int var, int var2) ;
 
 #endif
