b4dc02014613daced68d10e391e0284b9b4be929
thierry <thierry@ab02c66d-5113-476d-a8b6-74e7035bd63a>
2012-01-10 15:36:50 +0000

    use google sparse hash also as replacement for std hash_set
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/libddd/trunk@2826 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- src/util/hash_set.hh
+++ src/util/hash_set.hh
@@ -4,11 +4,17 @@
 #define GCC_VERSION (__GNUC__ * 10000 \
                 + __GNUC_MINOR__ * 100 \
                    + __GNUC_PATCHLEVEL__)
+
+
+#ifndef USE_STD_HASH
+#include <google/sparse_hash_set>
+#else
 /* Test for GCC <= 4.3.0 */
 #if GCC_VERSION < 40300
 #include <ext/hash_set>
 #else
 #include <tr1/unordered_set>
+#endif
 #endif
 
 #include "util/configuration.hh"
@@ -24,10 +30,14 @@ template
     >
 struct hash_set
 {
+#ifndef USE_STD_HASH
+  typedef typename google::sparse_hash_set<Key,Hash,Compare,Allocator> type;
+#else
 #if GCC_VERSION < 40300
   typedef typename __gnu_cxx::hash_set<Key,Hash,Compare,Allocator> type;
 #else
   typedef typename std::tr1::unordered_set<Key,Hash,Compare,Allocator> type;
+#endif
 #endif
 };
     
