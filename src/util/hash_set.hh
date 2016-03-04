c3276ff74645374d9d9fd0867ea3939b01cd4bda
mcolange <mcolange@ab02c66d-5113-476d-a8b6-74e7035bd63a>
2016-03-04 10:28:14 +0000

    Go c++11. Remove old tr1 includes. Remove specific includes for archaic gcc4.3
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/libddd/trunk@3714 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- src/util/hash_set.hh
+++ src/util/hash_set.hh
@@ -1,20 +1,10 @@
 #ifndef _HASH_SET_HH_
 #define _HASH_SET_HH_
 
-#define GCC_VERSION (__GNUC__ * 10000 \
-                + __GNUC_MINOR__ * 100 \
-                   + __GNUC_PATCHLEVEL__)
-
-
 #ifndef USE_STD_HASH
 #include <google/sparse_hash_set>
 #else
-/* Test for GCC <= 4.3.0 */
-#if GCC_VERSION < 40300
-#include <ext/hash_set>
-#else
-#include <tr1/unordered_set>
-#endif
+#include <unordered_set>
 #endif
 
 #include "util/configuration.hh"
@@ -33,11 +23,7 @@ struct hash_set
 #ifndef USE_STD_HASH
   typedef typename google::sparse_hash_set<Key,Hash,Compare,Allocator> type;
 #else
-#if GCC_VERSION < 40300
-  typedef typename __gnu_cxx::hash_set<Key,Hash,Compare,Allocator> type;
-#else
-  typedef typename std::tr1::unordered_set<Key,Hash,Compare,Allocator> type;
-#endif
+  typedef typename std::unordered_set<Key,Hash,Compare,Allocator> type;
 #endif
 };
     
