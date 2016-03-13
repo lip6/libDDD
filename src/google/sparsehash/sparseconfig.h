1814f3810da10d09753a86363de75a6a6ad34ffc
Maximilien Colange <colange@lrde.epita.fr>
2016-03-13 21:34:59 +0000

    Use c++11.
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/libddd/trunk@3763 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- src/google/sparsehash/sparseconfig.h
+++ src/google/sparsehash/sparseconfig.h
@@ -6,22 +6,11 @@
 /* Namespace for Google classes */
 #define GOOGLE_NAMESPACE ::google
 
-#if __cplusplus>=201103L
 /* the location of the header defining hash functions */
 #define HASH_FUN_H <functional>
 
 /* the namespace of the hash<> function */
 #define HASH_NAMESPACE std
-
-#else
-/* the location of the header defining hash functions */
-#define HASH_FUN_H <tr1/functional>
-
-/* the namespace of the hash<> function */
-#define HASH_NAMESPACE std::tr1
-
-#endif
-
 
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
