9ff70844f3d552aa38ac05bb5b56272962d41162
mcolange <mcolange@ab02c66d-5113-476d-a8b6-74e7035bd63a>
2016-03-13 21:34:59 +0000

    Use c++11.
    git-svn-id: https://projets-systeme.lip6.fr/svn/research/libddd/libddd/trunk@3763 ab02c66d-5113-476d-a8b6-74e7035bd63a

--- src/util/hash_set.hh
+++ src/util/hash_set.hh
@@ -1,22 +1,11 @@
 #ifndef _HASH_SET_HH_
 #define _HASH_SET_HH_
 
-#define GCC_VERSION (__GNUC__ * 10000 \
-                + __GNUC_MINOR__ * 100 \
-                   + __GNUC_PATCHLEVEL__)
-
 #ifndef USE_STD_HASH
-#  include <google/sparse_hash_set>
+#include <google/sparse_hash_set>
 #else
-#  if GCC_VERSION < 40300
-#    include <ext/hash_set>
-#  elif __cplusplus>=201103L
-#    include <unordered_set>
-#  else
-#    include <tr1/unordered_set>
-#  endif
+#include <unordered_set>
 #endif
-
 
 #include "util/configuration.hh"
 
@@ -34,13 +23,7 @@ struct hash_set
 #ifndef USE_STD_HASH
   typedef typename google::sparse_hash_set<Key,Hash,Compare,Allocator> type;
 #else
-#  if GCC_VERSION < 40300
-	typedef typename __gnu_cxx::hash_set<Key,Hash,Compare,Allocator> type;
-#  elif __cplusplus>=201103L
-	typedef typename std::unordered_set<Key,Hash,Compare,Allocator> type;	
-#  else
-	typedef typename std::tr1::unordered_set<Key,Hash,Compare,Allocator> type;
-#  endif
+  typedef typename std::unordered_set<Key,Hash,Compare,Allocator> type;
 #endif
 };
     
