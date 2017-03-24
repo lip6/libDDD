cda97d36201843e05c67eec742014c559fd38e02
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-03-24 20:23:52 +0100

    Bump version number to 1.9.0 due to reverse incompatible changes in include schema

--- tag.sh
+++ tag.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 DD=`date +'%Y%m%d%H%M%S'`
-sed -i  configure.ac -e "s/1\.8\.1/1.8.1.$DD/"
+sed -i  configure.ac -e "s/1\.9\.0/1.9.0.$DD/"
 cd website
 sed -i  index.html -e "s/DATETIME/$DD/"
 cd ..
